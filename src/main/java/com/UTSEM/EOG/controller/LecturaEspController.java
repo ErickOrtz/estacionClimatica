eb72d2a50dbe2fe52d596f0a5f717271c1f80d0b
Erick Ortiz <eo9977572@gmail.com>
2025-04-02 20:06:27 -0600

    api prueba
    agregando una ruta de prueba

--- src/main/java/com/UTSEM/EOG/controller/LecturaEspController.java
+++ src/main/java/com/UTSEM/EOG/controller/LecturaEspController.java
@@ -8,6 +8,7 @@ import com.UTSEM.EOG.dto.LecturaESPdto;
 import com.UTSEM.EOG.service.LecturaEspService;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -18,13 +19,17 @@ public class LecturaEspController {
 
 	@Autowired
 	LecturaEspService lecturaEspService;
-	
+
+	@GetMapping("/prueba")
+	public String pruebaApi() {
+		return "API funcionando correctamente";
+	}
+
 	@PostMapping("guardarLectura")
 	public String guardarLecturaESP32(@RequestBody LecturaESPdto lecturaESPdto) {
 
 		return lecturaEspService.guardarLectura(lecturaESPdto);
-		
+
 	}
-	
-	
+
 }
